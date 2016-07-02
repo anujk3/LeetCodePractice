4f935c2ec12c86ea21b3d2e4bc78329f04237861
Anuj Katiyal <anujk3@users.noreply.github.com>
2016-07-02 12:24:12 +0530

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.py
+++ 15-3Sum/solution.py
@@ -9,7 +9,7 @@ class Solution(object):
         result = []
 
         for i in range(0, len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:   # Main condition to prevent 
+            if i > 0 and nums[i] == nums[i-1]:   # Main condition to prevent duplicates of first number
                 continue
 
             front = i + 1
@@ -26,10 +26,10 @@ class Solution(object):
                     curr_list = [nums[i], nums[front], nums[back]]
                     result.append(curr_list)
 
-                    while front < back and nums[front] == nums[front+1]:
+                    while front < back and nums[front] == nums[front+1]: # Condition to prevent duplicates of second value in tuple
                         front += 1
-                    while front < back and nums[back] == nums[back-1]:
-                        back -= 1
+                    while front < back and nums[back] == nums[back-1]: # Condition to prevent duplicates in third value of tuple
+                        back -= 1 
                     front += 1
                     back -= 1
 
